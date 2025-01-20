9bcc478cc766297f5aa4b4549bbdc348a42594ba
DaniloHenrique <keltoitech@gmail.com>
2025-01-19 23:19:54 -0300

    feat.: melhorias e atualizações de pacotes

--- index.js
+++
@@ -1,1 +0,0 @@
-class Cthulhu{constructor(e,t,...n){new Map(Object.entries(t)).forEach(((t,c)=>{this[c]=n.includes(c)?t:t instanceof Array?t.map((t=>new e(t))):new e(t)}))}}export{Cthulhu};
--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ export default {
     ],
     output:[
         {
-            file:'index.js',
+            file:'dist/index.js',
             format:'es'
         }
     ]
